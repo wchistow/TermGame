199c40e3e1bb4f57b6418eca5526888f4849bc74
wchistow <wchistow@yandex.ru>
2022-01-22 13:03:18 +0300

    change assertion check

--- termgame/color.py
+++ termgame/color.py
@@ -1,5 +1,5 @@
 def from_rgb_to_string(r: int, g: int, b: int) -> str:
-    assert 0 <= r >= 255 and 0 <= g >= 255 and 0 <= b >= 255
+    assert 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255
 
     if 0 <= r <= 20 and 0 <= g <= 20 and 0 <= b <= 20:
         return '\033[0;30m'  # black
