8348f8532de44da6375f9928470adb94da6e1210
wchistow <wchistow@yandex.ru>
2022-01-23 12:28:06 +0300

    add arg bg_color and saving args

--- termgame/surface.py
+++ termgame/surface.py
@@ -1,10 +1,13 @@
 class Screen:
-    def __init__(self, width=80, height=24, color='\033[1;37m', symbol=' '):
+    def __init__(self, width=80, height=24, color='\033[1;37m', symbol=' ', bg_color='\033[0;40m'):
         self.width = width
         self.height = height
+        self.color = color
+        self.symbol = symbol
+        self.bg_color = bg_color
 
         self.start_line = 1
 
         # Draw empty board
         for y in range(self.height):
-            print(f'{color}{symbol}' * self.width)
+            print(f'{self.color}{self.symbol}' * self.width)
