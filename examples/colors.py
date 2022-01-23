e49e9f3e5a17717e0844d02dfa0f09c87e210993
wchistow <wchistow@yandex.ru>
2022-01-23 12:34:43 +0300

    add printing results

--- examples/colors.py
+++ examples/colors.py
@@ -1,4 +1,4 @@
 from termgame import color
 
-color.from_rgb_to_string(r=10, g=10, b=10)  # black
-color.from_rgb_to_string(r=240, g=240, b=240)  # white
+print(color.from_rgb_to_string(r=10, g=10, b=10) + '@')     # black
+print(color.from_rgb_to_string(r=240, g=240, b=240) + '@')  # white
