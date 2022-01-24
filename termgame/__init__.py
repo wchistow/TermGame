65f3bab7529a5dbcec41407c29d540b409fa007c
wchistow <wchistow@yandex.ru>
2022-01-24 13:53:04 +0300

    add OS check

--- termgame/__init__.py
+++ termgame/__init__.py
@@ -1,3 +1,12 @@
+import sys
+
+
+class InvalidOS(Exception): pass
+
+
+if sys.platform == 'win32':
+    raise InvalidOS("TermGame library can't work on Windows")
+
 from .surface import Screen
 
 __version__ = '0.1.0'
