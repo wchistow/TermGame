d1174e5db57fcdb1084b7447399a20f6837beee4
wchistow <wchistow@yandex.ru>
2022-01-29 11:46:54 +0300

    remove OS check

--- termgame/__init__.py
+++ termgame/__init__.py
@@ -1,12 +1,3 @@
-import sys
-
-
-class InvalidOS(Exception): pass
-
-
-if sys.platform == 'win32':
-    raise InvalidOS("TermGame library can't work on Windows")
-
 from .screen import Screen
 from .draw import *
 
