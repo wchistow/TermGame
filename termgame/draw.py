46ba4eeda694f60efc0c844abc8c031925c13ff8
wchistow <wchistow@yandex.ru>
2022-01-29 11:05:00 +0300

    add method rectangle to draw.py

--- termgame/draw.py
+++ termgame/draw.py
@@ -6,11 +6,11 @@ def line(x1: int, y1: int, x2: int, y2: int, screen, letter='▇'):
     dx = abs(max(x1, x2) - min(x1, x2))
     dy = abs(max(y1, y2) - min(y1, y2))
     if dx == 0:
-        for y in range(dy):
+        for y in range(y1, dy + 1):
             symbol(x1, y, screen, letter=letter)
         return
     if dy == 0:
-        for x in range(dx):
+        for x in range(x1, dx + 1):
             symbol(x, y1, screen, letter=letter)
         return
     x, y = x1, y1
@@ -22,3 +22,18 @@ def line(x1: int, y1: int, x2: int, y2: int, screen, letter='▇'):
             symbol(x, y, screen, letter=letter)
             y += 1
         x += 1
+
+
+def rectangle(x, y, width, height, screen, outline_letter='▇', fill_letter=' '):
+    line(x, y, x + width + 1, y, screen, letter=outline_letter)
+    current_x, current_y = x, y + 1
+    for _ in range(height - 2):
+        symbol(current_x, current_y, screen, letter=outline_letter)
+        current_x += 1
+        for _ in range(width - 2):
+            symbol(current_x, current_y, screen, letter=fill_letter)
+            current_x += 1
+        symbol(current_x, current_y, screen, letter=outline_letter)
+        current_x = x
+        current_y += 1
+    line(x, y + height - 1, x + width + 1, y + height - 1, screen, letter=outline_letter)
