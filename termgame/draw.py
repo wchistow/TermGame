d523141c713f4b7807580844a3cef97b5d3c0c96
wchistow <wchistow@yandex.ru>
2022-01-29 11:49:41 +0300

    correct bag in function line in draw.py: don't draw revers line.

--- termgame/draw.py
+++ termgame/draw.py
@@ -3,37 +3,38 @@ def symbol(x: int, y: int, screen, letter='▇'):
 
 
 def line(x1: int, y1: int, x2: int, y2: int, screen, letter='▇'):
-    dx = abs(max(x1, x2) - min(x1, x2))
-    dy = abs(max(y1, y2) - min(y1, y2))
+    dx = abs(x1 - x2)
+    dy = abs(y1 - y2)
     if dx == 0:
-        for y in range(y1, dy + 1):
+        if y2 < y1:
+            y1, y2 = y2, y1
+        for y in range(y1, y2 + 1):
             symbol(x1, y, screen, letter=letter)
         return
     if dy == 0:
-        for x in range(x1, dx + 1):
+        if x2 < x1:
+            x1, x2 = x2, x1
+        for x in range(x1, x2 + 1):
             symbol(x, y1, screen, letter=letter)
         return
     x, y = x1, y1
     step = round(dy / dx)
-    while x != x2 + 1 and y != y2 + 1:
+    step_x = 1 if x2 > x1 else -1
+    step_y = 1 if y2 > y1 else -1
+    while x != x2 + step_x and y != y2 + step_y:
         for _ in range(step):
-            if x > x2 and y > y2:
-                break
+            if x * step_x > x2 * step_x or y * step_y > y2 * step_y:
+                return
             symbol(x, y, screen, letter=letter)
-            y += 1
-        x += 1
+            y += step_y
+        x += step_x
 
 
 def rectangle(x, y, width, height, screen, outline_letter='▇', fill_letter=' '):
-    line(x, y, x + width + 1, y, screen, letter=outline_letter)
-    current_x, current_y = x, y + 1
-    for _ in range(height - 2):
-        symbol(current_x, current_y, screen, letter=outline_letter)
-        current_x += 1
-        for _ in range(width - 2):
+    line(x, y, x + width, y, screen, letter=outline_letter)
+    for current_y in range(y + 1, y + height):
+        symbol(x, current_y, screen, letter=outline_letter)
+        for current_x in range(x + 1, x + width):
             symbol(current_x, current_y, screen, letter=fill_letter)
-            current_x += 1
-        symbol(current_x, current_y, screen, letter=outline_letter)
-        current_x = x
-        current_y += 1
-    line(x, y + height - 1, x + width + 1, y + height - 1, screen, letter=outline_letter)
+        symbol(x + width, current_y, screen, letter=outline_letter)
+    line(x, y + height, x + width, y + height, screen, letter=outline_letter)
