84059ca4b05b9ddffeab4cd6bf020adb15feac1a
wchistow <wchistow@yandex.ru>
2022-02-04 12:56:15 +0300

    change __init__.py and add check of OS to it

--- termgame/__init__.py
+++ termgame/__init__.py
@@ -1,4 +1,15 @@
+from sys import platform
+
+
+class InvalidOS(Exception):
+    pass
+
+
+if platform == 'win32':
+    raise InvalidOS("TermGame library can't work on Windows.")
+
 from .screen import Screen
-from .draw import *
+from . import draw
+from . import color
 
 __version__ = 'alpha 0.1.0'
