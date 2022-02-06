06440d0bad8a787d4259fc53696d95411abab45d
wchistow <wchistow@yandex.ru>
2022-02-06 12:19:06 +0300

    add example keys.py

---
+++ examples/keys.py
@@ -0,0 +1,13 @@
+import termgame as tg
+
+running = True
+
+while running:
+    for evt in tg.events.get_events():
+        if evt == tg.events.K_ESC:
+            running = False
+            break
+        elif evt == tg.events.K_h:
+            print('Help')
+        else:
+            print(evt)
