7673671718682433611b16e41f58cfccaed98fbc
wchistow <wchistow@yandex.ru>
2022-02-10 14:50:43 +0300

    change file clock.py

--- termgame/clock.py
+++ termgame/clock.py
@@ -1,2 +1,16 @@
+import time
+
+
 class Clock:
-    pass
+    def __init__(self):
+        self.last_time = time.time()
+        self.fps = None
+
+    def tick(self, fps: int):
+        self._sec_per_tick = 1 / fps
+        if time.time() - self.last_time > self._sec_per_tick:
+            time.sleep((time.time() - self.last_time) - self._sec_per_tick)
+        self.last_time = time.time()
+
+    def get_fps(self) -> float:
+        return round(1 / self._sec_per_tick, 2)
