c79e9f3f9fc2023ced96e938b3de859231c28765
wchistow <wchistow@yandex.ru>
2022-02-13 12:04:33 +0300

    add method test_get_fps to class ClockTestCase in file test_clock.py

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -8,9 +8,13 @@ class ClockTestCase(unittest.TestCase):
     def setUp(self):
         self.testing_clock = tg.Clock()
 
-    def test_clock(self):
+    def test_tick(self):
         start = time.time()
         for _ in range(30):
             self.testing_clock.tick(30)
         finish = time.time()
         self.assertEqual(round(finish - start), 1)
+
+    def test_get_fps(self):
+        self.testing_clock.tick(30)
+        self.assertEqual(self.testing_clock.get_fps(), 30.0)
