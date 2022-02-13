716d0bbcce9680b7629be65cd35b07ff2e5f42f2
wchistow <wchistow@yandex.ru>
2022-02-13 11:48:51 +0300

    add file test_clock.py

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('/media/home/vladimir/Python/TermGame')
+
+from test_clock import ClockTestCase
+
 import unittest
 
 unittest.main()
---
+++ tests/test_clock.py
@@ -0,0 +1,11 @@
+import termgame as tg
+
+import unittest
+
+
+class ClockTestCase(unittest.TestCase):
+    def setUp(self):
+        self.testing_clock = tg.Clock()
+
+    def test_clock(self):
+        pass
