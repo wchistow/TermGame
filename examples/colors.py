cbd419b499d3fba853393e2c7115128cb1a550c9
wchistow <wchistow@yandex.ru>
2022-02-13 12:33:37 +0300

    add comment to file examples/colors.py

--- examples/colors.py
+++ examples/colors.py
@@ -4,7 +4,7 @@ screen = tg.Screen()
 
 for x, name in enumerate(dir(tg.color), 2):
     if not name.startswith('__') and not name == 'join':
-        symbol = tg.color.join('A ', eval('tg.color.' + name))
-        tg.draw.symbol(x, 2, screen, symbol)
+        symbol = tg.color.join('A ', eval('tg.color.' + name))  # Draw symbol "A" with all colors
+        tg.draw.symbol(x, 2, screen, symbol)                    # of module termgame.color.
 
 screen.draw()
