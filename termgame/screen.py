91db96b63ff0fca88f039dc6168b694ba1e5e02e
wchistow <wchistow@yandex.ru>
2022-02-14 16:59:44 +0300

    change screen.board from bytearray to list

--- termgame/screen.py
+++ termgame/screen.py
@@ -4,44 +4,45 @@ from sys import stdout
 
 class Screen:
     """Provides screen of game."""
-    def __init__(self, symbol=' ', color='', bg=''):
+    def __init__(self, size=None, symbol=' ', color='', bg=''):
         self.bg = bg
         self.text_color = color
         self.symbol = bg + color + symbol
 
-        size = get_terminal_size()
-        self.rows, self.columns = size.lines + 1, size.columns
+        if size is None:
+            s = get_terminal_size()
+            size = s.lines, s.columns
+        self.rows, self.columns = size
 
-        self.board = UTF32Matrix(self.rows, self.columns)
-        self.empty()
+        self.board = Matrix(self.rows, self.columns, self.symbol)
 
     def empty(self):
         """Empty attribute board."""
         for y in range(self.rows):
-            for x in range(self.columns):
-                self.board[x, y] = self.symbol
-            self.board[self.columns, y] = '\n'
+            for x in range(self.columns - 1):
+                self.board[y, x] = self.symbol
+            self.board[y, self.columns - 1] = '\n'
 
     def draw(self):
         """Draws attribute board in screen."""
         stdout.write(f'\033[{self.board.height}A')  # move cursor to (0,0)
-        stdout.write(self.board.board.decode('utf-32'))
+        stdout.write(''.join(self.board.board))
 
 
-class UTF32Matrix:
-    """Provides matrix with using bytearray in encoding utf-32."""
-    def __init__(self, height, width):
+class Matrix:
+    """Provides matrix with using simple list."""
+    def __init__(self, height, width, symbol):
         self.height = height
         self.width = width
 
-        self.board = bytearray([0] * width * height * 4)
+        self.board = [symbol] * width * height
 
     def __getitem__(self, item: tuple[int, int]):
         col, row = item
         s = row * self.width + col
-        return self.board[s * 4 : s * 4 + 4].decode('utf-32')
+        return self.board[s]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: tuple[int, int], value):
         row, col = key
         s = row * self.width + col
-        self.board[s * 4: s * 4 + 4] = value.encode('utf-32')[4:]
+        self.board[s] = value
