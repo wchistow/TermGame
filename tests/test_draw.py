62cde8926130c854d3187dff55b7e07114ac2ce0
wchistow <wchistow@yandex.ru>
2022-02-17 13:04:54 +0300

    change file tests/test_draw.py

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -4,4 +4,14 @@ import unittest
 
 
 class DrawTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.screen = tg.Screen()
+
+    def test_symbol(self):
+        tg.draw.symbol(0, 0, self.screen, 'A')
+        self.assertEqual(self.screen.board[0, 0], 'A')
+
+    def test_line(self):
+        tg.draw.line(0, 0, 0, 10, self.screen, 'A')
+        for y in range(0, 11):
+            self.assertEqual(self.screen.board[0, y], 'A')
