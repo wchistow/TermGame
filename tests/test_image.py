beca9324ddd0d16740e97a16446ea490f33ddd92
wchistow <wchistow@yandex.ru>
2022-02-20 13:06:19 +0300

    change file tests/test_image.py

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,8 +1,28 @@
 """This is tests of module termgame.image"""
 import termgame as tg
 
+from os import remove
 import unittest
 
 
 class ImageTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.screen = tg.Screen()
+        self.img = tg.image.Image(0, 0, ' A ', self.screen)
+
+    def test_class_image(self):
+        self.img.draw()
+
+        self.assertEqual(self.screen.board[0, 0], ' ')
+        self.assertEqual(self.screen.board[1, 0], 'A')
+        self.assertEqual(self.screen.board[2, 0], ' ')
+
+    def test_func_load(self):
+        with open('some_test.txt', 'w') as f:
+            f.write('A')
+
+        img = tg.image.load('some_test.txt', self.screen)
+
+        self.assertEqual(img.text, 'A')
+
+        remove('some_test.txt')
