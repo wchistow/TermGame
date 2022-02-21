747399e1f6d1326c6b7b66b734aaf466810b71ea
wchistow <wchistow@yandex.ru>
2022-02-21 14:07:15 +0300

    add file tests/test_screen.py

--- tests/main.py
+++ tests/main.py
@@ -6,6 +6,7 @@ from test_color import ColorTestCase
 from test_draw import DrawTestCase
 from test_events import EventsTestCase
 from test_image import ImageTestCase
+from test_screen import ScreenTestCase
 
 import unittest
 
---
+++ tests/test_screen.py
@@ -0,0 +1,8 @@
+"""This is tests of module termgame.screen"""
+import termgame as tg
+
+import unittest
+
+
+class ScreenTestCase(unittest.TestCase):
+    pass
