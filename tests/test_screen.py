aafe78cc52dcaada76c3a2ba4dab57d9b05fb5cc
wchistow <wchistow@yandex.ru>
2022-02-21 14:18:21 +0300

    change file tests/test_screen.py

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -5,4 +5,14 @@ import unittest
 
 
 class ScreenTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.screen = tg.Screen()
+
+    def test_empty(self):
+        tg.draw.rectangle(0, 0, 5, 5, self.screen, fill_letter='A')
+
+        self.screen.empty()
+
+        for y in range(self.screen.rows):
+            for x in range(self.screen.columns):
+                self.assertIn(self.screen.board[x, y], [' ', '\n'])
