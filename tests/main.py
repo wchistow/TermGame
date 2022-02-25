9efaf174a6b03bc4bc8ef32e480ea146b11715f8
wchistow <wchistow@yandex.ru>
2022-02-25 11:40:13 +0300

    remove file tests/test_events.py

--- tests/main.py
+++ tests/main.py
@@ -4,7 +4,6 @@ sys.path.append('/media/home/vladimir/Python/TermGame')
 from test_clock import ClockTestCase
 from test_color import ColorTestCase
 from test_draw import DrawTestCase
-from test_events import EventsTestCase
 from test_image import ImageTestCase
 from test_screen import ScreenTestCase
 
