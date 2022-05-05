5463b42644b165278e47be8c93cc81f4400be867
wchistow <wchistow@yandex.ru>
2022-05-05 12:59:08 +0300

    add tests of method collide_point and collide_image to file tests/test_image.py

--- tests/test_image.py
+++ tests/test_image.py
@@ -17,6 +17,14 @@ class ImageTestCase(unittest.TestCase):
         self.assertEqual(self.screen.board[1, 0], 'A')
         self.assertEqual(self.screen.board[2, 0], ' ')
 
+    def test_collide_point(self):
+        self.assertTrue(self.img.collide_point(1, 0))
+
+    def test_collide_image(self):
+        test_img = tg.image.Image(1, 0, 'A', self.screen)
+
+        self.assertTrue(self.img.collide_image(test_img))
+
     def test_func_load(self):
         with open('some_test.txt', 'w') as f:
             f.write('A')
