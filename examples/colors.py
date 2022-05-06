86afbabe0f32e224fd5492f86c24819380d96056
wchistow <wchistow@yandex.ru>
2022-05-06 13:04:19 +0300

    change colors.py example

--- examples/colors.py
+++ examples/colors.py
@@ -3,8 +3,8 @@ import termgame as tg
 screen = tg.Screen()
 
 for x, name in enumerate(dir(tg.color), 2):
-    if not name.startswith('__') and not name == 'join':
-        symbol = tg.color.join('A ', eval('tg.color.' + name))  # Draw symbol "A" with all colors
-        tg.draw.symbol(x, 2, screen, symbol)                    # of module termgame.color.
+    if not name.startswith('__') and name != 'join':
+        symbol = tg.color.join('A', eval('tg.color.' + name)) + ' '  # Draw symbol "A" with all colors
+        tg.draw.symbol(x, 2, screen, symbol)                         # of module termgame.color.
 
 screen.draw()
